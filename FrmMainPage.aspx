756a8b9e2448895530803f409c67690d1984b2bb
KiHyeon-Hong <ghdrlgus96@gmail.com>
2020-11-06 15:50:44 +0900

    ASP.NET Notice Board init
    ASP.NET Notice Board init

---
+++ FrmMainPage.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmMainPage.aspx.cs" Inherits="HKHNoticeBoard.FrmMainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <br />
+            &nbsp;<asp:Image ID="Image1" runat="server" ImageUrl="~/PageImages/logo.png" Height="50px" Width="150px" />
+            <hr />
+        </header>
+        <main>
+
+        </main>
+        <footer style="background-color: #CCCCCC">
+            <hr />
+            <p>good?</p>
+        </footer>
+    </form>
+</body>
+</html>
---
+++ SQLQuery/NoticeBoardQuery.sql
@@ -0,0 +1,46 @@
+﻿create table Member (
+	userId int identity(1,1) primary key,
+	id nvarchar(20) not null,
+	pwd nvarchar(20) not null,
+	userName nvarchar(20) not null,
+	userEmail nvarchar(20) not null,
+	birthYear int not null,
+	birthMon int not null,
+	birthDay int not null,
+	phoneNum nvarchar(20) not null,
+	alarm int not null,
+	userProfile nvarchar(20) not null
+);
+
+create table Write (
+	writeId int identity(1,1) primary key,
+	category int not null,
+	title nvarchar(256) not null,
+	body ntext not null,
+	createDay date not null,
+	updateDay date,
+	emailAtt nvarchar(20),
+	userId int not null,
+	foreign key(userId) references Member(userId)
+);
+
+create table Comment (
+	commentId int identity(1,1) primary key,
+	writeId int not null,
+	userId int not null,
+	body nvarchar(256) not null,
+	parentCommentId int,
+	foreign key(writeId) references Write(writeId),
+	foreign key(userId) references Member(userId),
+	foreign key(commentId) references Comment(commentId)
+);
+
+select * from Member;
+select * from Write;
+select * from Comment;
+
+drop table Member;
+drop table Write;
+drop table Comment;
+
+select * from Member, Write where Member.userId = Write.userId;
