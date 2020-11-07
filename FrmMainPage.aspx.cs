e979f32802ec217884a498eba752a5ad6815a93e
KiHyeon-Hong <ghdrlgus96@gmail.com>
2020-11-07 20:28:49 +0900

    주석 삭제
    주석 삭제

--- FrmMainPage.aspx.cs
+++ FrmMainPage.aspx.cs
@@ -29,56 +29,6 @@ namespace HKHNoticeBoard
                 cate = cate.ToString();
 
 
-            //SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
-            //conn.Open();
-
-            //string selectSql = "select * from Member, Write where Member.userId = Write.userId";
-            //SqlCommand cmd = new SqlCommand(selectSql, conn);
-
-            //DataSet ds = new DataSet();
-            //SqlDataAdapter da = new SqlDataAdapter(cmd);
-
-            ////Fill로 채운다
-            //da.Fill(ds, "BoardList");
-
-            //string myBoard =
-            //    "<table border=\"1\" style=\"width: 70%; margin-left: auto; margin-right: auto; \"  class=\"table table-striped table-hover table-condensed table-bordered\">" +
-            //        "<tr style=\"text-align: center\" class=\"active\">" +
-            //            "<td>" +
-            //                "<b>제목</b>" +
-            //            "</td>" +
-            //            "<td>" +
-            //                "<b>작성자</b>" +
-            //            "</td>" +
-            //            "<td>" +
-            //                "<b>작성일</b>" +
-            //            "</td>" +
-            //        "</tr>";
-
-            //foreach (DataRow item in ds.Tables["BoardList"].Rows)
-            //{
-            //    myBoard +=
-            //        $"<tr>" +
-            //            $"<td>" +
-            //                //$"{item["title"].ToString()}" +
-            //                $"<a href='FrmDetailPage.aspx?wid={item["writeId"].ToString()}'>{item["title"].ToString()}</a>" +
-            //            $"</td>" +
-            //            $"<td style=\"text-align: center\">" +
-            //                "<img src=\"userProfiles/" + item["userProfile"] + "\" height=\"25px\" width=\"25px\">" +
-            //                $"{item["userName"].ToString()}" +
-            //            //"<asp:TextBox ID=\"test\" runat=\"server\"></asp:TextBox>" +
-            //            //"<a href='www.naver.com'>test</a>" +
-            //            $"</td>" +
-            //            $"<td style=\"text-align: center\">" +
-            //                $"{item["createDay"].ToString().Split(' ')[0]}" +
-            //            $"</td>" +
-            //        $"</tr>";
-            //}
-
-            //myBoard += "</table>";
-
-            //board.Text = myBoard;
-
             defaultPagination(cate, defaultWebSetting(cate, int.Parse(page), 10), 10);
             defaultSetting();
         }
@@ -104,28 +54,6 @@ namespace HKHNoticeBoard
                             "<span aria-hidden=\"true\">&laquo;</span>" +
                         "</a>" +
                     "</li>";
-
-
-            //< nav style = "text-align: center" >
-
-            //         < ul class="pagination">
-            //            <li>
-            //                <a href = "#" aria-label="Previous">
-            //                    <span aria-hidden="true">&laquo;</span>
-            //                </a>
-            //            </li>
-            //            <li><a href = "#" > 1 </ a ></ li >
-            //            < li >< a href="#">2</a></li>
-            //            <li><a href = "#" > 3 </ a ></ li >
-            //            < li >< a href="#">4</a></li>
-            //            <li><a href = "#" > 5 </ a ></ li >
-            //            < li >
-            //                < a href="#" aria-label="Next">
-            //                    <span aria-hidden="true">&raquo;</span>
-            //                </a>
-            //            </li>
-            //        </ul>
-            //    </nav>
         }
 
         private int defaultWebSetting(string cate, int page, int onePage)
@@ -163,6 +91,9 @@ namespace HKHNoticeBoard
                         "<td>" +
                             "<b>작성일</b>" +
                         "</td>" +
+                        "<td>" +
+                            "<b>조회수</b>" +
+                        "</td>" +
                     "</tr>";
 
             int count = 0;
@@ -186,6 +117,9 @@ namespace HKHNoticeBoard
                         $"</td>" +
                         $"<td style=\"text-align: center\">" +
                             $"{item["createDay"].ToString().Split(' ')[0]}" +
+                        $"</td>" +
+                        $"<td style=\"text-align: center\">" +
+                            $"{item["viewCount"].ToString().Split(' ')[0]}" +
                         $"</td>" +
                     $"</tr>";
                 }
